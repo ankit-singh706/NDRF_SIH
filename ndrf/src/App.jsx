f0fe27c52b8f682deada823e55dd0b26ff15995a
TheGreatFellow <shreyas42000@gmail.com>
2022-08-20 20:01:31 +0530

    incidents page layout done

--- ndrf/src/App.jsx
+++ ndrf/src/App.jsx
@@ -1,22 +1,24 @@
-import NavBar from './components/NavBar';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import Homepage from './section/Homepage';
-import Donation from './section/Donation';
+import NavBar from './components/NavBar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Homepage from './section/Homepage'
+import Donation from './section/Donation'
 import './App.css'
+import Incidents from './section/Incidents'
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <NavBar/>
-      
-        <Routes>
-          <Route path='/' element={<Homepage/>}></Route>
-          <Route path='/donation' element={<Donation/>}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                <NavBar />
+
+                <Routes>
+                    <Route path='/' element={<Homepage />}></Route>
+                    <Route path='/donation' element={<Donation />}></Route>
+                    <Route path='/incidents' element={<Incidents />}></Route>
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App
