ee4d787c355cde525f5950badcc8c259b3d9ef05
Sreeniketh Madgula <sreeniketh.madgula@gmail.com>
2022-08-25 02:21:51 +0530

    added a stable version of disaster contracts; need to be refined

---
+++ contracts/Disaster.sol
@@ -0,0 +1,59 @@
+ // SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import './Incidents.sol';
+import './User.sol';
+import './SupplyChain.sol';
+
+contract Disaster {
+    struct DisasterInfo {
+        uint id;
+        uint timestamp;
+        string name;
+        string description;
+        string disasterType;
+        uint severity;
+        string location;
+    }
+    DisasterInfo disasterInfo;
+
+    struct DisasterStats {
+        uint deathCount;
+        uint affectedCount;
+        string[] affectedStates;
+    }
+    DisasterStats disasterStats;
+
+    Incidents incidents;
+    // User user;
+
+    event DisasterCreated(DisasterInfo disasterInfo, address incidents);
+    event IncidentCreated(Incidents.IncidentInfo incidentInfo);
+    
+    constructor(DisasterInfo memory _disasterInfo, Incidents _incidents) {
+        // user = _user;
+        incidents = _incidents;
+        disasterInfo = _disasterInfo;
+        // emit DisasterCreated(disasterInfo,address(incidents));
+    }
+
+    // modifier onlyAuthorized(address _user,uint _rank) {
+    //     require(user.getRank(_user)!=0,"Only ranked officials can access this method");
+    //     require(user.getRank(_user) >= _rank, "Not Authorized");
+    //     _;
+    // }
+
+    function getDisasterInfo() public view returns (DisasterInfo memory){
+        return disasterInfo;
+    }
+
+
+    function createIncident(Incidents.IncidentInfo memory _incidentInfo) public {
+        incidents.createIncident(_incidentInfo);
+        emit IncidentCreated(_incidentInfo);
+    }
+
+    function getIncidents() public view returns (Incidents) {
+        return incidents;
+    }
+}
