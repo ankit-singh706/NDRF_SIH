338660e6aae17b53ecd5b104e12cbe6db1dff79f
TheGreatFellow <shreyas42000@gmail.com>
2022-08-24 20:13:32 +0530

    some important changes

--- ndrf/src/components/Modal.jsx
+++ ndrf/src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import disaster from '../assets/disaster.png'
+import backIcon from '../assets/back.png'
 const Modal = ({ handleClose, show, children }) => {
     const showHideClassName = show
         ? 'modal display-block'
@@ -8,10 +9,31 @@ const Modal = ({ handleClose, show, children }) => {
     return (
         <div className={showHideClassName}>
             <section className='modal-main'>
-                {children}
+                {/* {children}
+
                 <button type='button' onClick={handleClose}>
                     Close
-                </button>
+                </button> */}
+                <div className='modal-left-section'>
+                    <div className='modal-top-section'>
+                        <img
+                            src={backIcon}
+                            alt='back'
+                            width='35px'
+                            className='back'
+                        />
+                        <h1 className='modal-heading'>
+                            Village of Khurda drowned in flood
+                        </h1>
+                    </div>
+                    <div className='media-container'>
+                        <img src={disaster} alt='disaster' className='media' />
+                    </div>
+                    <div className='location'>
+                        Khurda,Odisha | 20.5° N/ 89.7° E
+                    </div>
+                </div>
+                <div className='modal-right-section'></div>
             </section>
         </div>
     )
