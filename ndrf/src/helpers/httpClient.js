86da42e53885331880d72c00f808af78b4bf9e64
ankit-cs706 <ankit8938@gmail.com>
2022-08-26 04:54:07 +0530

    Integrating backend

---
+++ ndrf/src/helpers/httpClient.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+const httpClient = axios.create({
+    baseURL: 'http://localhost:3000',
+    withCredentials: true,
+})
+
+export default httpClient;
--- ndrf/src/section/Homepage.jsx
+++ ndrf/src/section/Homepage.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import styled from 'styled-components';
 import AboutSection from '../components/AboutSection';
 import DonationsContainer from '../components/DonationsContainer';
 import Statistics from '../components/Statistics';
-
+import httpClient from '../helpers/httpClient';
 
 const Homepage = (props) =>{
+
+    const [podList,setPodList] = useState();
+
+    const getPodList = async () => {
+        await httpClient({
+        url: '/people',
+        method: "get",
+    }).then((response) => {
+        var templist = response.data
+        console.log(templist)
+        setPodList(templist)
+    }).catch((error) => {
+        console.log(error);
+    });
+    }
+    useEffect(() => {
+    getPodList();
+ 
+
+}, [])
     return(
         <Container>
             <DonationsContainer/>
