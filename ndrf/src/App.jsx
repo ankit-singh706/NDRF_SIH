afc2a25ac93a1b717fa7ce6da1942e617d0f075d
TheGreatFellow <shreyas42000@gmail.com>
2022-08-25 18:53:47 +0530

    resolved conflict

--- ndrf/src/App.jsx
+++ ndrf/src/App.jsx
@@ -3,33 +3,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './section/Homepage'
 import Donation from './section/Donation'
 import './App.css'
-import Logistics from './section/Logistics';
-import Footer from './components/Footer';
-import Incidents from './section/Incidents';
+import Logistics from './section/Logistics'
+import Footer from './components/Footer'
+import Incidents from './section/Incidents'
 import { Dropdown } from 'bootstrap'
 import Chloropeth from './section/Chloropeth'
 import State from './section/State'
-
+import Disaster from './section/Disaster'
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <NavBar/>
-      
-        <Routes>
-          <Route path='/' element={<Homepage/>}></Route>
-          <Route path='/donation' element={<Donation/>}></Route>
-          <Route path='/map' element={<Chloropeth/>}></Route>
-          <Route path='/logistics' element={<Logistics/>}></Route>
-          <Route path='/incidents' element={<Incidents />}></Route>
-          <Route path='/drop' element={<Dropdown/>}></Route>
-          <Route path='/state' element={<State/>}></Route>
-        </Routes>
-        <Footer/>
-      </Router>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                <NavBar />
 
+                <Routes>
+                    <Route path='/' element={<Homepage />}></Route>
+                    <Route path='/donation' element={<Donation />}></Route>
+                    <Route path='/map' element={<Chloropeth />}></Route>
+                    <Route path='/logistics' element={<Logistics />}></Route>
+                    <Route path='/incidents' element={<Incidents />}></Route>
+                    <Route path='/drop' element={<Dropdown />}></Route>
+                    <Route path='/disaster' element={<Disaster />}></Route>
+                    <Route path='/state' element={<State />}></Route>
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    )
 }
 
 export default App
