e5b4aa88b95f0dd74f8e72f3a0e63d85c76b7e9e
TheGreatFellow <shreyas42000@gmail.com>
2022-08-26 16:35:04 +0530

    minor modification

--- ndrf/src/section/State.jsx
+++ ndrf/src/section/State.jsx
@@ -1,13 +1,14 @@
-import React,{useState,useEffect} from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import httpClient from '../helpers/httpClient';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import httpClient from '../helpers/httpClient'
+import image7 from '../assets/image 7.png'
+import image8 from '../assets/image 8.png'
+import image9 from '../assets/image 9.png'
 
-const State = (props) =>{
+import axios from 'axios'
 
-
-    
+const State = (props) => {
     const [disasterList, setDisasterList] = useState([])
     const getPodList = async () => {
         await httpClient({
@@ -27,108 +28,136 @@ const State = (props) =>{
         getPodList()
     }, [])
 
-    const navigate = useNavigate();
-    return(
+    const navigate = useNavigate()
+    return (
         <>
+            <div className='topmargin'></div>
             <GeographicalData>
-                <div className="temp"></div>
+                <div className='temp'>
+                    <img
+                        src={image7}
+                        alt=''
+                        className='statsp'
+                        draggable={false}
+                    />
+                    <img
+                        src={image8}
+                        alt=''
+                        className='statsp'
+                        draggable={false}
+                    />
+                    <img
+                        src={image9}
+                        alt=''
+                        className='statsp'
+                        draggable={false}
+                    />
+                </div>
             </GeographicalData>
-        
+
             <Heading>
                 {/* state is dynamic here */}
                 <h2>State Disaster Information</h2>
             </Heading>
-            {disasterList.map((disaster,index)=>{
-                return(
+            {disasterList.map((disaster, index) => {
+                return (
                     <Data>
-                <div className="info" key={index}>
-                    <div className="date">{disaster.Period}</div>
-                    
-                    <div className="disaster"><a onClick={()=>{navigate('/disaster',{state:disaster})}}>{disaster.Event_type}</a></div>
-                </div>
-                <hr/>
-            </Data>
+                        <div className='info' key={index}>
+                            <div className='date'>{disaster.Period}</div>
+
+                            <div className='disaster'>
+                                <a
+                                    onClick={() => {
+                                        navigate('/disaster', {
+                                            state: disaster,
+                                        })
+                                    }}
+                                >
+                                    {disaster.Event_type}
+                                </a>
+                            </div>
+                        </div>
+                        <hr />
+                    </Data>
                 )
-                
             })}
-        
         </>
     )
 }
 
-
 const Heading = styled.div`
     margin-top: 110px;
-    padding:20px 50px;
-    font-family: "Noto Sans", sans-serif;
+    padding: 20px 50px;
+    font-family: 'Noto Sans', sans-serif;
     display: flex;
     align-items: center;
     justify-content: center;
-    
-    h2{
-        font-size:2.0rem;
+
+    h2 {
+        font-size: 2rem;
         width: 90%;
         border-radius: 8px;
         padding: 3px 20px;
-        background-color: #00DCE6;
+        background-color: #00dce6;
     }
-`;
+`
 
 const GeographicalData = styled.div`
-    
-    .temp{
-        border:1px solid black;
-        height:50vh;
-        margin:20px 100px;
-        border-radius:20px;
+    .temp {
+        height: 50vh;
+        margin: 20px 100px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
     }
 
-    .club{
-        height:45vh;
-        display:flex;
+    .club {
+        height: 45vh;
+        display: flex;
         align-items: center;
         justify-content: center;
-        margin:20px 100px;
-        padding:10px 10px;
-        gap:30px; 
+        margin: 20px 100px;
+        padding: 10px 10px;
+        gap: 30px;
     }
 
-    .rain,.humid{
-        border:1px solid red;
-        padding:10px 10px;
-        height:inherit;
-        width:50%;
-        border-radius:20px;
+    .rain,
+    .humid {
+        border: 1px solid red;
+        padding: 10px 10px;
+        height: inherit;
+        width: 50%;
+        border-radius: 20px;
     }
 `
 
 const Data = styled.div`
     margin-bottom: 10px;
 
-    .info{
+    .info {
         display: flex;
-        flex-direction: row;  
-        padding:10px 130px;
-        font-family: "Noto Sans", sans-serif;
+        flex-direction: row;
+        padding: 10px 130px;
+        font-family: 'Noto Sans', sans-serif;
         justify-content: space-between;
-        margin:0 50px;
+        margin: 0 50px;
     }
 
-    .date,.disaster{
+    .date,
+    .disaster {
         font-size: 1.1rem;
         font-weight: 600;
         text-align: left;
-
     }
 
-    .disaster a{
+    .disaster a {
         color: black;
         text-decoration: underline;
     }
 
-    hr{
+    hr {
         margin: 0px 120px;
     }
 `
 
-export default State;
+export default State
